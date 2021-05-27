a8ed9a0c7fa300e2c5bdda3f9a209959e16f605f
Ryan Lawrence <volpanicstudios@gmail.com>
2021-05-27 15:47:13 +1000

    Imported spring struct, edited sample scene.

---
+++ 4plus1Carnival/Assets/Scripts/Structures/Spring.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Springs a float value between two values
+/// </summary>
+public struct Spring
+{
+    public float Value;
+    private float velocity;
+
+    public Spring(float initalValue)
+    {
+        Value = initalValue;
+        velocity = 0;
+    }
+
+    public float UpdateSpring(float targetValue, float stiffness, float damping, float valueThresh, float velocityThresh)
+    {
+        float dampfactor = Mathf.Max(0, 1 - damping * Time.deltaTime);
+        float accel = (targetValue - Value) * stiffness * Time.deltaTime;
+        velocity = velocity * dampfactor + accel;
+        Value += velocity * Time.deltaTime;
+
+        if(Mathf.Abs(Value - targetValue) < valueThresh &&
+            Mathf.Abs(velocity) < velocityThresh)
+        {
+            Value = targetValue;
+            velocity = 0f;
+        }
+
+        return Value;
+    }
+
+}
