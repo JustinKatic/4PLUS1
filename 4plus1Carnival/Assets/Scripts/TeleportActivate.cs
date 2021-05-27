67aea78b7277964a5ae055b484d2600ab5d2d0e0
Justin <justinkatic4@gmail.com>
2021-05-27 16:53:38 +1000

    testing teleport

---
+++ 4plus1Carnival/Assets/Scripts/TeleportActivate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class TeleportActivate : MonoBehaviour
+{
+    public GameObject teleportController;
+    public InputActionReference teleportActivateRefrence;
+
+
+    [Header("Teleport Events")]
+    public UnityEvent onTeleportActivate;
+    public UnityEvent onTeleportCancel;
+
+    void Start()
+    {
+        teleportActivateRefrence.action.performed += TeleportModeActivate;
+        teleportActivateRefrence.action.canceled += TeleportModeCancel; ;
+
+    }
+
+
+    private void TeleportModeActivate(InputAction.CallbackContext obj) => onTeleportActivate.Invoke();
+
+    private void TeleportModeCancel(InputAction.CallbackContext obj) => Invoke("DelayTeleportation", .1f);
+
+    private void DelayTeleportation() => onTeleportCancel.Invoke();
+
+
+
+}
