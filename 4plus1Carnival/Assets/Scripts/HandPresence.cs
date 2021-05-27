6d495d3a3f7c82cf3f9b607f13498d7f4e667e6a
Ryan Lawrence <volpanicstudios@gmail.com>
2021-05-27 16:31:36 +1000

    Midwork on VR hands, needs testing before further work.

---
+++ 4plus1Carnival/Assets/Scripts/HandPresence.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public InputDeviceCharacteristics DeviceCharecteristics;
+    public GameObject ModelPrefab;
+
+    private InputDevice targetDevice;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        //Get controllers with charecteristics, Don't know if the list is replaced or added too, we'll find out
+        InputDevices.GetDevicesWithCharacteristics(DeviceCharecteristics, devices);
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            if(ModelPrefab != null)
+            {
+                Instantiate(ModelPrefab, transform);
+            }
+        }
+    }
+}
