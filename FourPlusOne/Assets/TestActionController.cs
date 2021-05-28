8b6c065c93e566654194b05c4e0f3c35a88bcd68
Justin <justinkatic4@gmail.com>
2021-05-28 16:51:49 +1000

    UPDATED TO NEW UNITY VERSION

---
+++ FourPlusOne/Assets/TestActionController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class TestActionController : MonoBehaviour
+{
+
+    private ActionBasedController controller;
+
+    void Start()
+    {
+        controller = GetComponent<ActionBasedController>();
+
+        controller.selectAction.action.performed += Action_performed;
+    }
+
+
+    private void Action_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        Debug.Log("Button was pushed");
+    }
+
+}
