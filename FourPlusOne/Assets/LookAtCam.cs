ee4a1ab0f1c04fb057732bddde325b8de4625485
Justin <justinkatic4@gmail.com>
2021-06-04 09:13:43 +1000

    fixxed comic text look at

--- FourPlusOne/Assets/LookAtCam.cs
+++ FourPlusOne/Assets/LookAtCam.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class LookAtCam : MonoBehaviour
 {
 
-    public Transform cam; 
     private void OnEnable()
     {
         transform.LookAt(Camera.main.transform);
-        Debug.Log(cam.position);
     }
 }
