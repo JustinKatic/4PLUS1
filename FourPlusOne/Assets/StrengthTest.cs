8467503027adca77d10f80e21114a3a3717b2daf
Ryan Lawrence <volpanicstudios@gmail.com>
2021-06-09 14:57:37 +1000

    Ryan - Added strength test prefab and script.

---
+++ FourPlusOne/Assets/StrengthTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrengthTest : MonoBehaviour
+{
+    [Tooltip("The thing that goes up when hit, i don't know the propper name!")]
+    public Rigidbody Indecator;
+    public float StrengthScale = 1;
+
+    public void OnPressurePlateSlapped(SlapDetection slap)
+    {
+        if(Indecator.IsSleeping())
+        {
+            Indecator.AddForce(Vector3.up * slap.PreviousHitVelocity.magnitude* StrengthScale, ForceMode.Impulse);
+            Debug.Log(slap.PreviousHitVelocity.magnitude.ToString());
+        }
+    }
+}
