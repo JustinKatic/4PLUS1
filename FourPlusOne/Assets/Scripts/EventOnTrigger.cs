0a45440b8ac72eb10bcf4e8fc6731dcbbf92416e
Elric Milton <elric.milton@yahoo.com.au>
2021-06-09 16:49:58 +1000

    Updated dialogue box to face player when activated

--- FourPlusOne/Assets/Scripts/EventOnTrigger.cs
+++ FourPlusOne/Assets/Scripts/EventOnTrigger.cs
@@ -8,11 +8,14 @@ public class EventOnTrigger : MonoBehaviour
     public bool OnlyOnce = true;
     public UnityEvent OnEnterTrigger;
 
+    public GameObject dialogueBox;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             OnEnterTrigger.Invoke();
+            dialogueBox.transform.LookAt(other.transform);
             if (OnlyOnce) enabled = false;
         }
     }
