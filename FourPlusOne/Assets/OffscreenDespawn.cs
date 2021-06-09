5c684b65d1be35176ab4bb478d13897242eb348e
Ryan Lawrence <volpanicstudios@gmail.com>
2021-06-10 09:07:18 +1000

    Ryan - Made letters from VFX text despawn after offscreen for a time.

---
+++ FourPlusOne/Assets/OffscreenDespawn.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffscreenDespawn : MonoBehaviour
+{
+    [Tooltip("If we should destroy the object, or deactivate it")]
+    public bool UseDestroy = true;
+    public bool DestroyParent = false;
+    [Min(1)]
+    public int ParentDepth = 0;
+    private IEnumerator destoryRoutine;
+
+    private void OnBecameInvisible()
+    {
+        destoryRoutine = DeswpawnAfterTime();
+        StartCoroutine(destoryRoutine);
+        Debug.Log("invis");
+    }
+
+    private void OnBecameVisible()
+    {
+        if (destoryRoutine != null) StopCoroutine(destoryRoutine);
+        destoryRoutine = null;
+    }
+
+    IEnumerator DeswpawnAfterTime()
+    {
+        yield return new WaitForSeconds(5f);
+
+        //Destroy correct object
+        GameObject obj = gameObject;
+        if (DestroyParent)
+        {
+            for (int i = 0; i < ParentDepth; i++)
+            {
+                if (obj.transform.parent != null)
+                    obj = obj.transform.parent.gameObject;
+            }
+        }
+
+        if (UseDestroy) Destroy(obj);
+        else gameObject.SetActive(false);
+    }
+}
