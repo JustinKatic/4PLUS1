d4bb364fa71488a03310eaa07444460cdaa8a6d9
Ryan Lawrence <volpanicstudios@gmail.com>
2021-06-17 09:04:00 +1000

    Ryan - Added player dying from wave enemies, needs to be tested.

--- FourPlusOne/Assets/AngryPersonSpawner.cs
+++ FourPlusOne/Assets/AngryPersonSpawner.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class AngryPersonSpawner : MonoBehaviour
 {
     public Transform Target;
     public GameObject AngryPersonPrefab;
 
+    public UnityEvent OnPlayerFailEvent;
+
     private void Start()
     {
         AngryPerson angry = AngryPersonPrefab.GetComponent<AngryPerson>();
-        if (angry != null) angry.TargetTransform = Target;
+        if (angry != null)
+        {
+            angry.TargetTransform = Target;
+            angry.WithTransform = OnPlayerFailEvent;
+        }
+        OnPlayerFailEvent.Invoke();
     }
 
     // Update is called once per frame
@@ -21,5 +29,10 @@ public class AngryPersonSpawner : MonoBehaviour
             //SpawnPrefab
             Instantiate(AngryPersonPrefab,transform.position + new Vector3(Random.Range(-2,2),0, Random.Range(-2, 2)),Quaternion.identity);
         }
+    }
+
+    public void MoveTargetToTransform(Transform teleportPoint)
+    {
+        Target.position = teleportPoint.position;
     }
 }
