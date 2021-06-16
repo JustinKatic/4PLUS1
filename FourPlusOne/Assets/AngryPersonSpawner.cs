2e49b0df6fa2b4b635385cd5d9fb7cfc32fe3333
Ryan Lawrence <volpanicstudios@gmail.com>
2021-06-17 09:38:46 +1000

    Ryan - Edited angry persons spawner to fade out when player dies, still needs to be tested.

--- FourPlusOne/Assets/AngryPersonSpawner.cs
+++ FourPlusOne/Assets/AngryPersonSpawner.cs
@@ -7,8 +7,12 @@ public class AngryPersonSpawner : MonoBehaviour
 {
     public Transform Target;
     public GameObject AngryPersonPrefab;
+    public Fade FadeController;
 
     public UnityEvent OnPlayerFailEvent;
+    public UnityEvent OnFadededOut;
+
+    private bool done = false;
 
     private void Start()
     {
@@ -29,10 +33,36 @@ public class AngryPersonSpawner : MonoBehaviour
             //SpawnPrefab
             Instantiate(AngryPersonPrefab,transform.position + new Vector3(Random.Range(-2,2),0, Random.Range(-2, 2)),Quaternion.identity);
         }
+
+        if(done)
+        {
+            if(FadeController.FadeDone)
+            {
+                OnFadededOut.Invoke();
+                enabled = false;
+            }
+        }
     }
 
     public void MoveTargetToTransform(Transform teleportPoint)
     {
         Target.position = teleportPoint.position;
+    }
+
+    public void ChangeTargetTransform(Transform newTarget)
+    {
+        Target = newTarget;
+        AngryPerson angry = AngryPersonPrefab.GetComponent<AngryPerson>();
+        if (angry != null)
+        {
+            angry.TargetTransform = Target;
+            angry.WithTransform = OnPlayerFailEvent;
+        }
+    }
+
+    public void WaveDone()
+    {
+        done = true;
+        FadeController.FadeOut();
     }
 }
